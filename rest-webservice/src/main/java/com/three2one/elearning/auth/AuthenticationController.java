740d6795870610517c8d6ad01485f1c398372cff
msrashed2018 <msrashed100@gmail.com>
2019-05-26 02:27:58 +0200

    course-registration

---
+++ rest-webservice/src/main/java/com/three2one/elearning/auth/AuthenticationController.java
@@ -0,0 +1,29 @@
+package com.three2one.elearning.auth;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.three2one.elearning.entity.Student;
+import com.three2one.elearning.repository.StudentRepository;
+
+//Controller
+@CrossOrigin(origins="*")
+@RestController
+public class AuthenticationController {
+	@Autowired
+	private StudentRepository studentRepository;
+	private static final Logger logger = LoggerFactory.getLogger(SecurityServiceImpl.class);
+
+	@GetMapping(path = "/api/authenticate")
+	public Student authenticate(Authentication authentication) {
+		Student student = studentRepository.findByUsername(authentication.getName());
+		return student;
+	}
+
+}
